96c8cd95da785545c9f879d97a2d4b9464d593d3
tzlehtin <tero.lehtinen@louhisoftware.com>
2025-09-25 06:24:18 +0300

    Email luku toimii

---
+++ src/main/java/ws/lumo/api/provider/AdapterConfigurationProvider.java
@@ -0,0 +1,18 @@
+package ws.lumo.api.provider;
+
+import java.util.List;
+
+/**
+ * Defines a contract for providing adapter configurations to the adapter library.
+ * The implementation of this interface will reside in the main service application
+ * and will be responsible for fetching configurations from the database.
+ */
+public interface AdapterConfigurationProvider {
+
+    /**
+     * Finds all adapter configurations of a specific type.
+     * @param type The type of the adapter (e.g., "EMAIL_ADAPTER").
+     * @return A list of configurations for the given type.
+     */
+    List<AdapterConfig> findConfigurationsByType(String type);
+}
