50ce78d4bb5bff4086a9c6fa11ad2df065988a05
tzlehtin <tero.lehtinen@louhisoftware.com>
2025-11-16 01:00:17 +0200

    Sposti 3 osapuoli korjaus

--- src/main/java/ws/lumo/ai/orchestration/AiInteractionService.java
+++ src/main/java/ws/lumo/ai/orchestration/AiInteractionService.java
@@ -2,13 +2,17 @@ package ws.lumo.ai.orchestration;
 
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
+import ws.lumo.ai.ChatMessage;
+
+import java.util.List;
 
 /**
- * Responsible for the direct interaction with the AI model, including
- * tool registration and execution.
+ * The service responsible for the direct interaction with the AI model,
+ * including tool registration and execution.
  */
 public interface AiInteractionService {
-
-    ChatResponse getAiResponse(String adapterId, Prompt prompt);
-
+    /**
+     * Gets a response from the AI model.
+     */
+    ChatResponse getAiResponse(String adapterId, Prompt prompt, List<ChatMessage> history);
 }
